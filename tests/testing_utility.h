1d0f82d91bf5cf070c95064b93620ceff5c2b3e9
Dmitrii Umnov <umnovdimitry@icloud.com>
2024-03-13 23:53:47 +0100

    Add fermat and miller-rabin benchmarks

--- tests/testing_utility.h
+++ tests/testing_utility.h
@@ -6,19 +6,10 @@
 #include <optional>
 
 #include "common.h"
+#include "concepts.h"
 #include "data.h"
 
 namespace LargePrimeNumbers {
-
-template <typename T>
-concept PrimalityTest = requires(T func, const bigint& arg) {
-  { func(arg) } -> std::same_as<bool>;
-};
-
-template <typename T>
-concept FindFactor = requires(T func, const bigint& arg) {
-  { func(arg) } -> std::same_as<std::optional<bigint>>;
-};
 
 inline void set_log_level_for_tests() {
   namespace logging = boost::log;
