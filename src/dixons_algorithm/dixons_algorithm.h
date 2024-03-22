c6381a3e8c795e8e946e6ab328061dbef75fc8b2
Dmitrii Umnov <umnovdimitry@icloud.com>
2024-03-22 23:56:13 +0100

    Add CFRAC

--- src/dixons_algorithm/dixons_algorithm.h
+++ src/dixons_algorithm/dixons_algorithm.h
@@ -12,28 +12,33 @@ namespace LargePrimeNumbers {
 
 class DixonsAlgorithm {
 public:
-  explicit DixonsAlgorithm(const bigint& n,
-                           const std::vector<size_t>& factor_base,
-                           int fill_factor = 3)
+  explicit DixonsAlgorithm(
+      const bigint& n, const std::vector<size_t>& factor_base,
+      int fill_factor = 1)
       : n_(n),
         factor_base_(factor_base),
+        number_of_factored_candidates_required_(
+            factor_base.size() * fill_factor + 3),
         factorized_candidates_{},
-        matrix_(fill_factor * factor_base.size() + 1,
-                (1 + fill_factor) * factor_base.size() + 1),
+        matrix_(
+            number_of_factored_candidates_required_ + 1,
+            number_of_factored_candidates_required_ + factor_base_.size()),
         factorization_powers_cache_(factor_base_.size()) {}
 
   void process_candidates(std::span<const bigint> candidates);
 
   // Checks if g(r) = r * r mod n can be factorized over factor base, and if so,
   // saves factorization powers to matrix for further gaussian elimination
-  void process_candidate(const bigint& candidate);
+  bool process_candidate(const bigint& candidate);
 
   std::optional<bigint> find_factor();
+
+  bool factored_enough_candidates() const;
 
 private:
   bigint perform_polynomial_factorization_over_factor_base(const bigint& r);
 
-  void factorize_over_factor_base_and_fill_powers_matrix(const bigint& r);
+  bool factorize_over_factor_base_and_fill_powers_matrix(const bigint& r);
 
   // Checks if number corresponding to i-th row of matrix can be used to find a
   // factor of n
@@ -41,6 +46,7 @@ private:
 
   bigint n_;
   std::vector<size_t> factor_base_;
+  size_t number_of_factored_candidates_required_;
   std::vector<bigint> factorized_candidates_;
   F2Matrix matrix_;
 
--- tests/test_find_factors.cpp
+++ tests/test_find_factors.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 
+#include "find_factor_cfrac.h"
 #include "find_factor_quadratic_sieve.h"
 #include "pollard_rho_find_factor.h"
 #include "pollard_rho_one_find_factor.h"
@@ -15,8 +16,8 @@ const bigint kBigNumberThreshold = boost::multiprecision::pow(bigint(10), 30);
 TEST(TrialFindFactorTest, test) {
   const static std::set<bigint> exclusions = {};
 
-  test_find_factor(find_factor_trial, "find_factor_trial", exclusions,
-                   std::nullopt, 10);
+  test_find_factor(
+      find_factor_trial, "find_factor_trial", exclusions, std::nullopt, 10);
 }
 
 TEST(PollardRhoFindFactorTest, test) {
@@ -26,8 +27,9 @@ TEST(PollardRhoFindFactorTest, test) {
   auto test_find_factor_pollard_rho = [&kTestParams](const bigint &n) {
     return find_factor_pollard_rho(n, kTestParams);
   };
-  test_find_factor(test_find_factor_pollard_rho, "find_factor_pollard_rho",
-                   kExclusions, std::nullopt, kBigNumberThreshold);
+  test_find_factor(
+      test_find_factor_pollard_rho, "find_factor_pollard_rho", kExclusions,
+      std::nullopt, kBigNumberThreshold);
 }
 
 TEST(PollardRhoOneFindFactorTest, test) {
@@ -40,20 +42,33 @@ TEST(PollardRhoOneFindFactorTest, test) {
   auto test_find_factor_pollard_rho_one = [&kTestParams](const bigint &n) {
     return find_factor_pollard_rho_one(n, kTestParams);
   };
-  test_find_factor(test_find_factor_pollard_rho_one,
-                   "find_factor_pollard_rho_one", kExclusions, std::nullopt,
-                   kBigNumberThreshold);
+  test_find_factor(
+      test_find_factor_pollard_rho_one, "find_factor_pollard_rho_one",
+      kExclusions, std::nullopt, kBigNumberThreshold);
 }
 
 TEST(QuadraticSieveFactorTest, test) {
-  constexpr auto kTestParams = QuadraticSieveParams{2000, 50000000, 2.0};
+  constexpr auto kTestParams = QuadraticSieveParams{5000, 100000000, 2.0};
   const std::set<bigint> kExclusions = {};
 
   auto test_find_factor_quadratic_sieve = [&kTestParams](const bigint &n) {
     return find_factor_quadratic_sieve(n, kTestParams);
   };
 
-  test_find_factor(test_find_factor_quadratic_sieve,
-                   "find_factor_quadratic_sieve", kExclusions,
-                   kBigNumberThreshold, std::nullopt);
+  test_find_factor(
+      test_find_factor_quadratic_sieve, "find_factor_quadratic_sieve",
+      kExclusions, kBigNumberThreshold, std::nullopt);
+}
+
+TEST(CfracFactorTest, test) {
+  constexpr auto kTestParams = CfracParams{500};
+  const std::set<bigint> kExclusions = {};
+
+  auto test_find_factor_cfrac = [&kTestParams](const bigint &n) {
+    return find_factor_cfrac(n, kTestParams);
+  };
+
+  test_find_factor(
+      test_find_factor_cfrac, "find_factor_cfrac", kExclusions,
+      kBigNumberThreshold, std::nullopt);
 }
