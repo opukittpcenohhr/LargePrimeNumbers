9faf513e4479f7564f3cfbdec445ce54c3993943
Dmitrii Umnov <umnovdimitry@icloud.com>
2024-03-29 12:14:04 +0100

    Refactor plot_stats_by_bases_count.h

--- src/concepts.h
+++ src/concepts.h
@@ -1,17 +1,26 @@
 #pragma once
 
+#include <span>
+
 #include "common.h"
 
 namespace LargePrimeNumbers {
 
 template <typename T>
-concept PrimalityTest = requires(T func, const bigint& arg) {
+concept PrimalityTest = requires(T&& func, const bigint& arg) {
   { func(arg) } -> std::same_as<bool>;
 };
 
 template <typename T>
-concept FindFactor = requires(T func, const bigint& arg) {
+concept FindFactor = requires(T&& func, const bigint& arg) {
   { func(arg) } -> std::same_as<std::optional<bigint>>;
+};
+
+template <typename T>
+concept PrimalityTestWithBasesCount = requires(
+    T&& func, std::span<const int> bases,
+    const LargePrimeNumbers::bigint& arg) {
+  { func(arg, bases) } -> std::same_as<bool>;
 };
 
 }  // namespace LargePrimeNumbers
